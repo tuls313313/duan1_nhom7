3f1ee2cc7411eb074d68d8c913788f55abb07aa7
tuls313313 <pcls313313@gmail.com>
2024-11-09 17:44:01 +0700

    up

--- index.php
+++ index.php
@@ -6,15 +6,8 @@
 require_once './controllers/user/HomeUserController.php';
 require_once './controllers/admin/HomeAdminController.php';
 
-// // Require toàn bộ file Controllers
-// require_once './controllers/HomeController.php';
-
-// // Require toàn bộ file Models
-// require_once './models/Student.php';
 $homeUser = new HomeUserController();
 $homeAdmin = new HomeAdminController;
-
-// Route
 
 // Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
 
