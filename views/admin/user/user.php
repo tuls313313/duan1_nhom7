b53bff931da6c821b64d22f5b0fd5349f25e66b3
tuls313313 <pcls313313@gmail.com>
2024-11-11 20:54:57 +0700

    changer

--- views/admin/user/user.php
+++ views/admin/user/user.php
@@ -138,7 +138,7 @@
 <script>
   $(function () {
     $("#example1").DataTable({
-      "responsive": true, "lengthChange": false, "autoWidth": false,"order": [],
+      "responsive": true, "lengthChange": false, "autoWidth": false,
       "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
     $('#example2').DataTable({
