0c40faeacb2f2f133db14c9957334f3b5d96e727
Hinh021205 <hinhnvph52863@gmail.com>
2024-11-15 00:24:40 +0700

    update index

--- index.php
+++ index.php
@@ -6,22 +6,26 @@ require_once './commons/database.php';
 
 //user
 require_once './controllers/user/userController.php';
-require_once './controllers/admin/userController.php';
-require_once './controllers/admin/orderController.php';
+// require_once './controllers/admin/userController.php';
+// require_once './controllers/admin/orderController.php';
 require_once './controllers/user/userController.php';
 
 
 //controller
 require_once './controllers/admin/orderController.php';
 require_once './controllers/admin/userController.php';
+require_once './controllers/admin/categoriesController.php';
+
 
 //models
 require_once './models/admin/userModels.php';
 require_once './models/admin/orderModel.php';
+require_once './models/admin/categoriesModel.php';
 
 $User = new HomeUserController();
 $Admin = new HomeAdminController;
 $AdminOrder = new OrderController();
+$AdminCategories = new CategoriesController();
 
 // Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
 
@@ -43,7 +47,12 @@ $response = match ($act) {
     'admin/order/edit' => $AdminOrder->editOrder(),
     'admin/order/editOrder' => $AdminOrder->postOrder(),
 
+    // Quản lý danh mục
+    'admin/categories' => $AdminCategories->getAllCategories(),
+    'admin/categories/edit' => $AdminCategories->editCategories(),
+    'admin/categories/editCategories' => $AdminCategories->postCategories(),
 
+    // Quản lý tài khoản
     'admin/user/add' =>$Admin->insertUser(),
     'admin/user/edit' => $Admin->editUser(),
     'admin/user/nextedit' => $Admin->nextedit(),
