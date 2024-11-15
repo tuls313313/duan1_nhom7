87c2fc1df36dd1b35efaeb4057002e06f2dc2422
Hinh021205 <hinhnvph52863@gmail.com>
2024-11-15 22:44:55 +0700

    updateModel

--- models/admin/categoriesModel.php
+++ models/admin/categoriesModel.php
@@ -9,23 +9,38 @@ class CategoriesModel
         $this->db = new Database();
     }
 
-    public function getAllCategories()
+    public function getAllCategory()
     {
         $sql = "SELECT * FROM categories";
         return $this->db->getAll($sql);
     }
 
-    public function getOneCategories($id)
+    public function getOneCategory($id)
     {
         $sql = "SELECT * FROM categories WHERE id = $id";
 
         return $this->db->getOne($sql);
     }
 
-    public function editCategories($id, $name, $status_categories)
+    public function editCategory($id, $name, $status_categories)
     {
         $sql = "UPDATE categories SET `name`='$name',`status_categories`='$status_categories' WHERE id = $id";
 
         return $this->db->insert($sql);
+    }
+
+    public function deleteCategory($id)
+    {
+        $sql = "DELETE FROM categories WHERE id = $id";
+
+        return $this->db->getOne($sql);
+    }
+
+    public function inserCategory($name, $status_categories)
+    {
+        $sql = "INSERT INTO categories (`name`, `status_categories`) VALUES ('$name','$status_categories')";
+        $sql1 = " INSERT INTO `categories`(`name`, `status_categories`) VALUES ('$name','$status_categories') ";
+        // var_dump($sql);die;
+        return $this->db->insert($sql1);
     }
 }
