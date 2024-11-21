e92ef6e0fb60aedd4bae4639cc5b6ba6920cfd4b
van <van2772005@gmail.com>
2024-11-21 10:06:31 +0700

    abc111

---
+++ models/admin/productModel.php
@@ -0,0 +1,46 @@
+<?php
+class AdminProductModels
+{
+    public $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getAllProduct()
+    {
+        $sql = "SELECT * FROM product order by id desc";
+        return $this->db->getAll($sql);
+    }
+
+    public function getOneProduct($id)
+    {
+        $sql = "SELECT * FROM product WHERE id=$id";
+        return $this->db->getOne($sql);
+    }
+    public function deleteProduct($id)
+    {
+        $sql = "DELETE FROM product WHERE id=$id";
+        return $this->db->getOne($sql);
+    }
+
+
+    public function insertProduct($name, $price, $img, $description, $id_categories)
+    {
+        $sql = "INSERT INTO `product`(`name`, `price`, `img`, `description`, `id_categories`) 
+        VALUES ('$name','$price','$img','$description','$id_categories')";
+        return $this->db->insert($sql);
+    }
+
+    public function editProduct($id, $name, $price, $img, $description, $id_categories, $status)
+    {
+        $sql = "UPDATE `product` SET `name`='$name',`price`='$price',
+        `img`='$img',`description`='$description',`id_categories`='$id_categories',
+        `status`='$status' WHERE id=$id ";
+        return $this->db->insert($sql);
+    }
+}
+
+
+
