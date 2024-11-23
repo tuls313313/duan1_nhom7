54ffddccd6b61977c28334f88cd5cc8b43c3c1ff
Tran Anh Van <trananhvan@Van-Anh-Tran.local>
2024-11-23 19:50:43 +0700

    sp

--- views/user/index.php
+++
@@ -1,9 +0,0 @@
-
-<?php include './views/user/layout/header.php'; ?>
-<!-- <div class="container"> -->
-
-<!-- <?php include './views/user/layout/slider.php'; ?> -->
-
-
-
-<?php include './views/user/layout/footer.php'; ?>
