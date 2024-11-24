984cfbcb236c77c82690dab1ec85e2b10f35e6be
Hinh021205 <hinhnvph52863@gmail.com>
2024-11-24 17:17:01 +0700

    lỗi validate fix sau

--- controllers/admin/orderController.php
+++ controllers/admin/orderController.php
@@ -49,10 +49,34 @@ class OrderController
             // var_dump(value: $_POST['editDonHang']); die;
             $id_order = $_GET['id_order'];
             $user_id = $_POST['user_id'];
-            $status_order = $_POST['status_order'];
-            if ($status_order == 3 && !$status_order == 2 && !$status_order == 1) {
-                $error[] = "ent";
-            } 
+
+            $status_order = intval($_POST['status_order']);
+            // if ($status_order == 1) {
+            //     $error[] = "Trạng thái mới không hợp lệ. Bạn chỉ có thể chọn 'Đang giao' hoặc 'Hoàn thành'.";
+            // } elseif ($status_order == 2) {
+            //     $error[] = "Trạng thái mới không hợp lệ. Bạn chỉ có thể chọn 'Hoàn thành'.";
+            // } elseif ($status_order == 3) {
+            //     $error[] = "Đơn hàng đã hoàn thành, không thể thay đổi trạng thái.";
+            // } else {
+            //     $error[] = "Trạng thái mới không hợp lệ.";
+            // }
+
+            // $status_order = $_POST['status_order'];
+            // if ($status_order == 3 && !$status_order == 2 && !$status_order == 1) {
+            //     $error[] = "ent";
+            // } 
+            
+            if ($status_order == 3) {
+                // Trạng thái 3 không thể chuyển thành 1 hoặc 2
+                $error[] = "Lỗi: Không thể thay đổi từ trạng thái 3 sang trạng thái 1 hoặc 2.";
+            } elseif ($status_order == 1 || $status_order == 2) {
+                // Trạng thái 1 và 2 có thể chuyển qua lại, hoặc chuyển lên 3
+                // Không có lỗi ở đây
+            } elseif ($status_order != 1 && $status_order != 2 && $status_order != 3) {
+                // Nếu trạng thái không phải là 1, 2 hoặc 3, báo lỗi
+                $error[] = "Trạng thái đơn hàng không hợp lệ.";
+            }
+
             // var_dump($status_order);die;
             $payment = $_POST['payment'];
             $total_amount = $_POST['total_amount'];
@@ -71,7 +95,7 @@ class OrderController
                 header("Location: ?act=admin/order&message=success");
             } else {
                 $_SESSION['error'] = $error;
-                header("Location: ?act=admin/order/edit&id=$id_order&message=error");
+                header("Location: ?act=admin/order&message=error");
             }
         } else {
             header("Location: ?act=admin/order&message=error.");
